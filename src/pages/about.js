5a9923276dbf0e8a36d9bf1b095fc5c8eb581aad
Gerardo Garza <owlscloud@gmail.com>
2023-03-29 02:19:52 -0500

    about page

---
+++ src/pages/about.js
@@ -0,0 +1,101 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import {
+  motionValue,
+  useInView,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref);
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+  return <span ref={ref}></span>;
+};
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Gerardo | About Page</title>
+        {/* SEO */}
+        <meta name="description" content="web portfolio" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Passion Fuels Purpose!" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                About Me
+              </h2>
+              <p className="font-medium">
+                Hi I'm Gerardo, feel free to call me Javi. I'm a Texas based
+                software engineer specializing in front‑end development focused
+                on crafting clean & user‑friendly experiences, I am passionate
+                about building excellent software that improves the lives of
+                those around me.
+              </p>
+            </div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark " />
+              <Image
+                src={profilePic}
+                alt="Me"
+                className="w-full h-auto rounded-2xl"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={999} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Lines of Code
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={15} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Repositories
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={3} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
