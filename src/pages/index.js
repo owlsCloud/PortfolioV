bb6db89b486f74caf9d6c89ea81e5ee380470352
Gerardo Garza <owlscloud@gmail.com>
2023-03-30 05:39:11 -0500

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,9 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2">
                 <Link
-                  href="/dummy.pdf"
+                  href="../../public/resume.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
-                  download={true}
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
