af8bd7a2df53a20e1f21dd3e9cc7b395af11c9fe
Gerardo Garza <owlscloud@gmail.com>
2023-03-31 04:43:32 -0500

    a

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -10,7 +10,7 @@ const HireMe = () => {
           className={"fill-dark animate-spin-slow dark:fill-light "}
         />
         <Link
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light  md:w-10 md:h-10 md:text-[10px]"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light  md:w-10 md:h-10 md:text-[10px] md:text-center"
           href="mailto:gjgarza31@gmail.com"
         >
           Hire Me
