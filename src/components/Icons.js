0ba21a0f18f892d47aaf90b76bb45054abe18cfa
Gerardo Garza <owlscloud@gmail.com>
2023-05-19 21:06:31 -0500

    a

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -76,7 +76,7 @@ export const SunIcon = ({ className, ...rest }) => (
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    viewBox="0 0 512 512"
+    viewBox="0 0 24 24"
     {...rest}
     className={`w-full h-auto ${className}`}
   >
@@ -254,7 +254,7 @@ export const MoonIcon = ({ className, ...rest }) => (
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    viewBox="0 0 512 512"
+    viewBox="0 0 24 24"
     {...rest}
     className={`w-full h-auto ${className}`}
   >
